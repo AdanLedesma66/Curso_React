387601f59092e807df6ccdbcd8650b0f8fb2400a
Adan Ledesma <adan.ledesma@somosetica.com>
2024-05-27 10:43:20 -0400

    usando create-react-app

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+
+// ::: constantes y variables
+
+const nombre = 'Adan';
+const apellido = 'Ledesma';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if(true){
+    const nombre = 'Peter';
+    console.log(nombre);
+}
+
+console.log(valorDado);
